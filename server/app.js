666b2770dcbfd536f348b396076ccaabd9129594
Ubuntu <ubuntu@ip-172-26-8-182.eu-west-1.compute.internal>
2026-02-18 12:42:55 +0000

    fix: update app.js and package-lock.json, remove SLACK_INTEGRATION.md

--- server/app.js
+++ server/app.js
@@ -9,10 +9,28 @@ const { port } = require('./config/env');
 const app = express();
 
 app.use(logger('dev'));
+
+// Configurar CORS
 app.use(cors({
-  origin: 'http://localhost:5000',
+  origin: [
+    'http://localhost:5000',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://skylab.labit.es',
+    'https://cloudadmin.labit.es'
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Cache-Control',
+    'Pragma',
+    'Expires'
+  ]
 }));
+
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
