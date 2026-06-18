79bc579c2312517eae2af841ef4dc55652d863aa
FranOrteg <ortegfran@gmail.com>
2026-06-18 13:43:19 +0200

    Fix: ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -17,5 +17,18 @@ module.exports = {
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+  }, {
+    name: 'MySQL_Tunnel',
+    script: '/home/ubuntu/server_SP_ACC/mysql-tunnel.sh',
+    interpreter: 'bash',
+    instances: 1,
+    exec_mode: 'fork',
+    autorestart: true,
+    watch: false,
+    restart_delay: 5000,
+    max_restarts: 50,
+    error_file: './logs/pm2-tunnel-error.log',
+    out_file: './logs/pm2-tunnel-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
   }]
 };
